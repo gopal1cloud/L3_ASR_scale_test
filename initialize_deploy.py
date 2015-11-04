9a53f6c428a4c4e964d4a903d1b7a3c86b1f95b4
sahanasj <sahanasj@onecloudinc.com>
2015-11-04 14:13:32 +0530

    Added try catch exception for ASR verification Report

--- initialize_deploy.py
+++ initialize_deploy.py
@@ -9,7 +9,7 @@ from neutronclient.v2_0 import client
 from credentials import get_credentials
 from config import OS_TENANT_NAME, NETWORK_NAME_PREFFIX, NETWORK_COUNT, \
     EXTERNAL_NETWORK, print_scale_test_config, DEPLOYMENT_ID, \
-    ASR_HOST, ASR_USER, ASR_PASSWORD
+    ASR_HOST, ASR_USER, ASR_PASSWORD, ENABLE_ASR_VERIFICATION
 from networks import create_network
 from prettytable import PrettyTable
 from asr import GetASRCmd
@@ -146,28 +146,37 @@ def main():
     network_vlan = {}
     for entry in test_data:
         network_vlan[str(entry['network_data']['network_vlan_id'])] = entry['network_data']['network_name'] 
-
-    vrf_router_id = router_id[:6]
-    vrfname = "nrouter"+'-'+vrf_router_id+'-'+DEPLOYMENT_ID
-
-    asr_verify_cmd = GetASRCmd(asr_host=ASR_HOST,
-                                     asr_host_port=22,
-                                     asr_user=ASR_USER,
-                                     asr_password=ASR_PASSWORD,
-                                     asr_slots=["0"])
-
-    router_data = asr_verify_cmd.get_router_detail(vrfname)
-    ipnatpool_data = asr_verify_cmd.get_ipnat_pool_detail(vrfname)
-    iproute_data = asr_verify_cmd.get_iproute_detail(vrfname)
-
-    interface_data = []
-    for interface in router_data['interfaces']:
-        interface_data.append(asr_verify_cmd.get_network_interface_detail(vrfname, interface))
-    nat_data = []
-    for interface in interface_data:
-        interfaceid = DEPLOYMENT_ID+'_'+interface['vlan_id']
-        nat_data.append(asr_verify_cmd.get_interface_nat_access_detail(interface['vlan_id'], interfaceid))
     
+    if ENABLE_ASR_VERIFICATION:
+        vrf_router_id = router_id[:6]
+        vrfname = "nrouter"+'-'+vrf_router_id+'-'+DEPLOYMENT_ID
+
+        asr_verify_cmd = GetASRCmd(asr_host=ASR_HOST,
+                                         asr_host_port=22,
+                                         asr_user=ASR_USER,
+                                         asr_password=ASR_PASSWORD,
+                                         asr_slots=["0"])
+        router_data = {'vrfname': vrfname, 'interfaces': '', 'status': ''}
+        ipnatpool_data = {'vrfname': vrfname, 'nat_pool_name': '', 'start_ip': '', 'end_ip': '', 'netmask': '', 'status': ''}
+        iproute_data = {'vrfname': vrfname, 'prefix': '', 'mask': '', 'interface': '', 'next_hop_address': '', 'status': ''}
+        interface_data = []
+        nat_data = []
+        try:
+            router_data = asr_verify_cmd.get_router_detail(vrfname)
+            ipnatpool_data = asr_verify_cmd.get_ipnat_pool_detail(vrfname)
+            iproute_data = asr_verify_cmd.get_iproute_detail(vrfname)      
+            for interface in router_data['interfaces']:
+                interface_data.append(asr_verify_cmd.get_network_interface_detail(vrfname, interface))
+            for interface in interface_data:
+                interfaceid = DEPLOYMENT_ID+'_'+interface['vlan_id']
+                nat_data.append(asr_verify_cmd.get_interface_nat_access_detail(interface['vlan_id'], interfaceid))
+            asr_report = True
+        except Exception as exc:
+            print "\n"
+            print "[ERROR] Caught exception on ASR Verification : %s" % (exc.message)
+            print "\n"
+            asr_report = False
+
     print "="*50
     print "\n"
     print "Scale Test Deployment Completed"
@@ -189,26 +198,27 @@ def main():
     print "            Instance Creation Results      "
     print print_instance_info(test_data)
     print "\n"
+    
+    if ENABLE_ASR_VERIFICATION and asr_report:
+        print "           OpenStack-ASR Router VRF Verification Results               "
+        print asr_router_vrf_info(router_name, router_data)
+        print "\n"
 
-    print "           OpenStack-ASR Router VRF Verification Results               "
-    print asr_router_vrf_info(router_name, router_data)
-    print "\n"
+        print "           OpenStack-ASR IP NAT Pool Verification Results               "
+        print asr_ipnat_pool_info(router_name, ipnatpool_data)
+        print "\n"
 
-    print "           OpenStack-ASR IP NAT Pool Verification Results               "
-    print asr_ipnat_pool_info(router_name, ipnatpool_data)
-    print "\n"
+        print "           OpenStack-ASR IP Route Verification Results               "
+        print asr_iproute_info(router_name, iproute_data)
+        print "\n"
 
-    print "           OpenStack-ASR IP Route Verification Results               "
-    print asr_iproute_info(router_name, iproute_data)
-    print "\n"
+        print "           OpenStack-ASR Network VRF Verification Results              "
+        print asr_network_vrf_info(router_name, network_vlan, interface_data)
+        print "\n"
 
-    print "           OpenStack-ASR Network VRF Verification Results              "
-    print asr_network_vrf_info(router_name, network_vlan, interface_data)
-    print "\n"
-
-    print "           OpenStack-ASR Network Interface's Dynamic NAT & Access list Entry Verification Results              "
-    print asr_interface_nat_info(router_name, network_vlan, nat_data)
-    print "\n"
+        print "           OpenStack-ASR Network Interface's Dynamic NAT & Access list Entry Verification Results              "
+        print asr_interface_nat_info(router_name, network_vlan, nat_data)
+        print "\n"
 
 
 if __name__ == '__main__':
